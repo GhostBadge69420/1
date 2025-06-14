1af70e5ddb818154708d5d4519bd546ace1ae1ab
Ghost_Badger_69420 <114076299+GhostBadge69420@users.noreply.github.com>
2025-06-14 18:04:17 +0530

    Delete keyword arguments directory

--- keyword arguments/greetings.py
+++
@@ -1,4 +0,0 @@
-def hello(greeting, title, first, last):
-    print(f"{greeting} {title}{first} {last}")
-
-hello("Hello", "Mr.", "Spongebob", "Squarepants")
--- keyword arguments/prefix_arguments.py
+++
@@ -1,4 +0,0 @@
-def hello(greeting, title, first, last):
-    print(f"{greeting} {title}{first} {last}")
-
-hello("Hello", title="Mr.", first="Spongebob", last="Squarepants")
