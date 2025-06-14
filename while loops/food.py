a8a59ceb5b55bd3258f0a471db0db842ca7b4d64
Ghost_Badger_69420 <114076299+GhostBadge69420@users.noreply.github.com>
2025-06-14 18:00:21 +0530

    Delete while loops directory

--- while loops/food.py
+++
@@ -1,7 +0,0 @@
-food = input("Enter a food you like (q to quit): ")
-
-while not food == "q":
-    print(f"You like {food}")
-    food = input("Enter another food you like (q to quit): ")
-
-print("bye")
--- while loops/infinite_loop.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter your name: ")
-
-while name == "":
-    print("You did not enter your name")
-print(f"Hello {name}")
